aa676c0e4c4bf2a91a5dea0e96c4b7b1aab26226
topping <reyna11@naver.com>
2024-11-06 17:51:14 +0900

    1106 과일장수 solution code 추가

--- Java/src/main/java/me/topping/AIReport/FruitSeller.java
+++ Java/src/main/java/me/topping/AIReport/FruitSeller.java
@@ -7,6 +7,25 @@ class Solution1 {
     int solution(int k, int m, int[] score) {
         int answer = 0;
 
+        // Solution Code
+//        Arrays.sort(score);
+//        for (int i = score.length; i >= m; i -= m) {
+//            answer += score[i - m] * m;
+//        }
+
+        // Solution Code2
+//        int[] sc2 = new int[k + 1];
+//        for (int i = 0; i < score.length; i++) sc2[score[i]]++;
+//
+//        int sum = 0, mod = 0;
+//        for (int i = k; i > 0; i--) {
+//            sc2[i] = sc2[i] + mod;
+//            sum += sc2[i] / m * m * i;
+//            mod = sc2[i] % m;
+//        }
+//
+//        return sum;
+
         Integer[] result = Arrays.stream(score).boxed().toArray(Integer[]::new);
         Arrays.sort(result, Collections.reverseOrder());
 
@@ -14,7 +33,7 @@ class Solution1 {
             int min = result[i + m - 1];
             answer += min * m;
         }
-        System.out.println(answer);
+        //System.out.println(answer);
         return answer;
     }
 }
