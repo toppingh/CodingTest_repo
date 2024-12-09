9a7dd7e17945cbe06ab41101d634eec7b889e309
topping <reyna11@naver.com>
2024-12-09 13:17:30 +0900

    1209 등차수열의 특정한 항만 더하기 솔루션 코드 추가

--- Java/Programmers/Day1209/EquidistantSequence.java
+++ Java/Programmers/Day1209/EquidistantSequence.java
@@ -3,6 +3,8 @@ package Java.Programmers.Day1209;
 // Title : 등차수열의 특정한 항만 더하기
 // Q. 첫째 항이 a, 공차가 d인 등차수열에서 included[i]가 i + 1항을 의미할때, 이 등차수열의 1항부터 n항까지 include가 true인 항들만 더한 값을 return하는 solution 함수
 
+import java.util.stream.IntStream;
+
 class Solution2 {
     int solution (int a, int d, boolean[] included) {
         int answer = 0;
@@ -11,6 +13,13 @@ class Solution2 {
             if (included[i] == true) answer += a + d * i;
         }
         return answer;
+    }
+}
+
+// Solution Code -> Stream 활용
+class SolutionCode2 {
+    int solution (int a, int d, boolean[] included) {
+        return IntStream.range(0, included.length).map(idx -> included[idx]?a+(idx * d) : 0).sum();
     }
 }
 
