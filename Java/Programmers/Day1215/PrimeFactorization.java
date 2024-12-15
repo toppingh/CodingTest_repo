dbf1dc739b2fd983ac0381978643d8b4147e7433
topping <reyna11@naver.com>
2024-12-15 20:04:29 +0900

    1215 소인수분해 (프로그래머스)

---
+++ Java/Programmers/Day1215/PrimeFactorization.java
@@ -0,0 +1,39 @@
+package Java.Programmers.Day1215;
+
+// Title : 소인수분해
+// Q. 소인수분해 : 어떤 수를 소수들의 곱으로 표현한 것
+// Q. 자연수 n이 매개변수일 때, n의 소인수를 오름차순으로 담은 배열을 return하는 solution 함수
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution4 {
+    int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 2; i <= n; i++) {
+            if (n % i == 0) {
+                while (n % i == 0) {
+                    n /= i;
+                }
+                list.add(i);
+            }
+        }
+        int[] answer = new int[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
+
+public class PrimeFactorization {
+    public static void main(String[] args) {
+        Solution4 s4 = new Solution4();
+        System.out.println(Arrays.toString(s4.solution(12)));
+        System.out.println(Arrays.toString(s4.solution(17)));
+        System.out.println(Arrays.toString(s4.solution(420)));
+    }
+}
