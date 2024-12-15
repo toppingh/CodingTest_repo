438e9ccb357b1c35946d09510d0fd8e21d1f0c53
topping <reyna11@naver.com>
2024-12-15 20:53:44 +0900

    1215 소인수분해 임포트 변경

--- Java/Programmers/Day1215/PrimeFactorization.java
+++ Java/Programmers/Day1215/PrimeFactorization.java
@@ -4,9 +4,7 @@ package Java.Programmers.Day1215;
 // Q. 소인수분해 : 어떤 수를 소수들의 곱으로 표현한 것
 // Q. 자연수 n이 매개변수일 때, n의 소인수를 오름차순으로 담은 배열을 return하는 solution 함수
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 class Solution4 {
     int[] solution(int n) {
