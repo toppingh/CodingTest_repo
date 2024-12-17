2e33dcd2a0d062893e4df92bc0388906e7e1e265
topping <reyna11@naver.com>
2024-12-17 13:35:04 +0900

    1217 카운트 업 (프로그래머스)

---
+++ Java/Programmers/Day1217/CountUp.java
@@ -0,0 +1,24 @@
+package Java.Programmers.Day1217;
+
+// Title : 카운트 업
+// Q. 정수 start_num와 end_num이 주어질 때, start_num부터 end_num까지의 숫자를 차례로 담은 리스트를 return하는 solution 함수
+
+import java.util.Arrays;
+
+class Solution3 {
+    int[] solution (int start_num, int end_num) {
+        int[] arr = new int[end_num - start_num + 1];
+
+        for (int i = start_num; i <= end_num; i++) {
+            arr[i - start_num] = i;
+        }
+        return arr;
+    }
+}
+
+public class CountUp {
+    public static void main(String[] args) {
+        Solution3 s3 = new Solution3();
+        System.out.println(Arrays.toString(s3.solution(3, 10)));
+    }
+}
