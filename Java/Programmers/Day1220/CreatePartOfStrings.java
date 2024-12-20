840f5980c9edc85f0a9093d109e5a8931b6a92f8
topping <reyna11@naver.com>
2024-12-20 17:43:02 +0900

    1220 부분 문자열 이어 붙여 문자열 만들기 (프로그래머스)

---
+++ Java/Programmers/Day1220/CreatePartOfStrings.java
@@ -0,0 +1,25 @@
+package Java.Programmers.Day1220;
+
+// Title : 부분 문자열 이어 붙여 문자열 만들기
+// Q. 길이가 같은 문자열 배열 my_strings와 이차원 정수 배열 parts가 매개변수로 주어진다.
+// Q. parts[i]는 [s, e] 형태로, my_string[i]의 인덱스 s부터 인덱스 e까지의 부분 문자열을 의미한다.
+// Q. 각 my_strings의 원소 parts에 해당하는 부분 문자열을 순서대로 이어 붙인 문자열을 return하는 solution 함수
+
+class Solution4 {
+    String solution(String[] my_strings, int[][] parts) {
+        String str = "";
+
+        for (int i = 0; i < my_strings.length; i++) {
+            str += my_strings[i].substring(parts[i][0], parts[i][1] + 1);
+            System.out.println(my_strings[i].substring(parts[i][0], parts[i][1] + 1));
+        }
+        return str;
+    }
+}
+
+public class CreatePartOfStrings {
+    public static void main(String[] args) {
+        Solution4 s4 = new Solution4();
+        System.out.println(s4.solution(new String[] {"progressive", "hamburger", "hammer", "ahocorasick"}, new int[][] {{0, 4}, {1, 2}, {3, 5}, {7, 7}}));
+    }
+}
