ae6b9816555e96cb02253746c035497a4ca1c1ec
topping <reyna11@naver.com>
2025-01-22 13:54:00 +0900

    0122 평행 (프로그래머스) 코드 수정

--- Java/Programmers/Year2025/Day0121/Parallel.java
+++ Java/Programmers/Year2025/Day0121/Parallel.java
@@ -7,9 +7,20 @@ package Java.Programmers.Year2025.Day0121;
 class Solution4 {
     int solution (int[][] dots) {
 
-        if ((dots[1][1] - dots[0][1]) * (dots[3][0] - dots[2][0]) == (dots[3][1] - dots[2][1]) * (dots[1][0] - dots[0][0])) {
+        double m1 = (double) (dots[1][1] - dots[0][1]) / (dots[1][0] - dots[0][0]);
+        double m2 = (double) (dots[3][1] - dots[2][1]) / (dots[3][0] - dots[2][0]);
+
+        if (Double.isInfinite(m1) || Double.isInfinite(m2) || m1 == m2) {
             return 1;
         }
+
+        double m3 = (double) (dots[2][1] - dots[0][1]) / (dots[2][0] - dots[0][0]);
+        double m4 = (double) (dots[3][1] - dots[1][1]) / (dots[3][0] - dots[1][0]);
+
+        if (Double.isInfinite(m3) || Double.isInfinite(m4) || m3 == m4) {
+            return 1;
+        }
+
         return 0;
     }
 }
