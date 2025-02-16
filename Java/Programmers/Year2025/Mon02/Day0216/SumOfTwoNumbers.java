76f1a9784894c933279ca2cf61da662762f961fd
topping <reyna11@naver.com>
2025-02-16 20:54:20 +0900

    0216 두 수의 합 솔루션 코드 추가

--- Java/Programmers/Year2025/Mon02/Day0216/SumOfTwoNumbers.java
+++ Java/Programmers/Year2025/Mon02/Day0216/SumOfTwoNumbers.java
@@ -14,11 +14,38 @@ class Solution2 {
     }
 }
 
+// SolutionCode -> 반복문 활용
+class SolutionCode2 {
+    String solution (String a, String b) {
+        StringBuilder sb = new StringBuilder();
+
+        int carry = 0; // 올림수 저장 변수
+        int i = a.length() - 1, j = b.length() - 1;
+
+        while (i >= 0 || j >= 0 || carry > 0) { // 남은 올림수가 없을 때까지
+            int sum = carry;
+
+            if (i >= 0) sum += a.charAt(i--) - '0'; // a 문자열 뒤에서부터 하나씩
+            if (j >= 0) sum += b.charAt(j--) - '0'; // b 문자열 뒤에서부터 하나씩
+            sb.append(sum % 10); // 자릿수 값 추가
+            carry = sum / 10; // 올림수 계산
+        }
+        // 결과는 뒤집어서 반환
+        return sb.reverse().toString();
+    }
+}
+
 public class SumOfTwoNumbers {
     public static void main(String[] args) {
         Solution2 s2 = new Solution2();
+        SolutionCode2 sc2 = new SolutionCode2();
+
         System.out.println(s2.solution("582", "734"));
         System.out.println(s2.solution("18446744073709551615", "287346502836570928366"));
         System.out.println(s2.solution("0", "0"));
+        System.out.println();
+        System.out.println(sc2.solution("582", "734"));
+        System.out.println(sc2.solution("18446744073709551615", "287346502836570928366"));
+        System.out.println(sc2.solution("0", "0"));
     }
 }
