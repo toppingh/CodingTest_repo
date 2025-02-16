5e5aeb99e0bd6ec7599b73d5c16ade9c38aefbc4
topping <reyna11@naver.com>
2025-02-16 20:31:19 +0900

    0216 0 떼기 (프로그래머스)

---
+++ Java/Programmers/Year2025/Mon02/Day0216/Delete0.java
@@ -0,0 +1,18 @@
+package Java.Programmers.Year2025.Mon02.Day0216;
+
+// Title : 0 떼기
+// Q. 정수로 이루어진 문자열 n_str의 가장 왼쪽에 처음으로 등장하는 0들을 뗀 문자열을 return 하는 solution 함수
+
+class Solution1 {
+    String solution (String n_str) {
+        return n_str.replaceAll("^0+", "");
+    }
+}
+
+public class Delete0 {
+    public static void main(String[] args) {
+        Solution1 s1 = new Solution1();
+        System.out.println(s1.solution("0010"));
+        System.out.println(s1.solution("854020"));
+    }
+}
