0f63bee3dae10c4316513e8b24f7e17194680f39
topping <reyna11@naver.com>
2025-02-20 09:11:54 +0900

    0220 [PCCE 기출문제] 1번 / 문자 출력 (프로그래머스)

---
+++ Java/Programmers/Year2025/Mon02/Day0220/PCCE1_PrintString.java
@@ -0,0 +1,17 @@
+package Java.Programmers.Year2025.Mon02.Day0220;
+import java.util.Scanner;
+// Title : [PCCE 기출문제] 1번 / 문자 출력
+// Q. 주어진 코드는 변수에 데이터를 저장하고 출력하는 코드입니다. 아래와 같이 출력되도록 빈칸을 채워 코드를 완성해주세요.
+// 3
+// 2
+// 1
+// Let's go!
+
+public class PCCE1_PrintString {
+    public static void main(String[] args) {
+        String message = "Let's go!";
+
+        System.out.println("3\n2\n1");
+        System.out.println(message);
+    }
+}
