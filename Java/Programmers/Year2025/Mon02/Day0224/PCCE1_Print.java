52422fa308a6a92da384a999b483426300081999
topping <reyna11@naver.com>
2025-02-24 09:52:05 +0900

    0224 [PCCE 기출문제] 1번 / 출력 (프로그래머스)

---
+++ Java/Programmers/Year2025/Mon02/Day0224/PCCE1_Print.java
@@ -0,0 +1,19 @@
+package Java.Programmers.Year2025.Mon02.Day0224;
+
+// Title : [PCCE 기출문제] 1번 / 출력
+// Q. 아래와 같이 출력 되도록 코드 작성
+// Spring is beginning
+// 13
+// 310
+
+public class PCCE1_Print {
+    public static void main(String[] args) {
+        String msg = "Spring is beginning";
+        int val1 = 3;
+        String val2 = "3";
+
+        System.out.println(msg);
+        System.out.println(val1 + 10);
+        System.out.println(val2 + "10");
+    }
+}
