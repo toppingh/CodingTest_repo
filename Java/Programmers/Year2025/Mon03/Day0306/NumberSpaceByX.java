7466541c04affe67defd5d70118b8098b44239ff
topping <reyna11@naver.com>
2025-03-06 09:38:16 +0900

    0306 x만큼 간격이 있는 n개의 숫자 (프로그래머스)

---
+++ Java/Programmers/Year2025/Mon03/Day0306/NumberSpaceByX.java
@@ -0,0 +1,35 @@
+package Java.Programmers.Year2025.Mon03.Day0306;
+
+// Title : x만큼 간격이 있는 n개의 숫자
+// Q. x의 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트 return 하는 solution 함수
+// Q. x는 -10000000 이상, 10000000 이하인 정수
+// Q. n은 1000 이하인 자연수
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution1 {
+    long[] solution (int x, int n) {
+        List<Long> list = new ArrayList<>();
+
+        for (int i = 1; i <= n; i ++) {
+            list.add((long) x * i);
+        }
+
+        long[] answer = new long[list.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
+
+public class NumberSpaceByX {
+    public static void main(String[] args) {
+        Solution1 s1 = new Solution1();
+        System.out.println(Arrays.toString(s1.solution(2, 5)));
+        System.out.println(Arrays.toString(s1.solution(4, 3)));
+        System.out.println(Arrays.toString(s1.solution(-4, 2)));
+    }
+}
