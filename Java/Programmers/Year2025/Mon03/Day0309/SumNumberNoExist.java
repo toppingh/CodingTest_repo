0c168d158c8a5f45efc63a712bd384639a7e4051
topping <reyna11@naver.com>
2025-03-09 18:15:05 +0900

    0309 없는 숫자 더하기 솔루션 코드 추가

--- Java/Programmers/Year2025/Mon03/Day0309/SumNumberNoExist.java
+++ Java/Programmers/Year2025/Mon03/Day0309/SumNumberNoExist.java
@@ -21,10 +21,28 @@ class Solution2 {
     }
 }
 
+// SolutionCode -> 0 ~ 9의 합에서 빼기
+class SolutionCode2 {
+    int solution (int[] numbers) {
+        int sum = 45; // 0 ~ 9까지의 합
+
+        for (int i : numbers) {
+            sum -= i;
+        }
+
+        return sum;
+    }
+}
+
 public class SumNumberNoExist {
     public static void main(String[] args) {
         Solution2 s2 = new Solution2();
+        SolutionCode2 sc2 = new SolutionCode2();
+
         System.out.println(s2.solution(new int[] {1, 2, 3, 4, 6, 7, 8, 0}));
         System.out.println(s2.solution(new int[] {5, 8, 4, 0, 6, 7, 9}));
+        System.out.println();
+        System.out.println(sc2.solution(new int[] {1, 2, 3, 4, 6, 7, 8, 0}));
+        System.out.println(sc2.solution(new int[] {5, 8, 4, 0, 6, 7, 9}));
     }
 }
