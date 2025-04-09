b8ef7321f52c00ee5c666a8f2911f11dd3eaf36a
topping <reyna11@naver.com>
2025-04-09 14:13:16 +0900

    0409 둘만의 암호 (프로그래머스)

---
+++ Java/Programmers/Year2025/Mon04/Day0409/CodeBetweenTwoOfUs.java
@@ -0,0 +1,37 @@
+package Java.Programmers.Year2025.Mon04.Day0409;
+
+// Title : 둘만의 암호
+// Q. 문자열 s, skip, 자연수 index가 주어질 때 아래 규칙에 따라 문자열을 만드려고 한다.
+// 1. 문자열 s의 각 알파벳을 index만큼 뒤의 알파벳으로 바꿔준다.
+// 2. index만큼의 뒤의 알파벳이 z를 넘어갈 경우 다시 a로 돌아간다.
+// 3. skip에 있는 알파벳은 제외하고 건너뛴다.
+// Q. 위 규칙대로 s를 변환한 결과를 return 하는 solution 함수
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution2 {
+    String solution (String s, String skip, int index) {
+        StringBuilder sb = new StringBuilder();
+        List<Character> list = new ArrayList<>();
+
+        for (char c = 'a'; c <= 'z'; c++) {
+            if (!skip.contains(String.valueOf(c))) {
+                list.add(c);
+            }
+        }
+
+        for (char c : s.toCharArray()) {
+            sb.append(list.get((list.indexOf(c) + index) % list.size()));
+        }
+
+        return sb.toString();
+    }
+}
+
+public class CodeBetweenTwoOfUs {
+    public static void main(String[] args) {
+        Solution2 s2 = new Solution2();
+        System.out.println(s2.solution("aukks", "wbqd", 5));
+    }
+}
